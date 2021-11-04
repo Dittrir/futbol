e9a81543e092cf36a3b76ffecfaaab18f76f9314
craig cardwell <wizeduvcc@gmail.com>
2021-11-03 20:50:42 -0600

    test: edit tests for average goals per season method

--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -36,15 +36,32 @@ RSpec.describe GameStats do
     expect(@game_stats.percentage_away_wins).to eq(33.33)
   end
 
-  xit "#percentage_ties" do
+  it "#percentage_ties" do
 
     expect(@game_stats.percentage_ties).to eq(11.11)
   end
 
   it "count_of_games_by_season" do
-
-    expect(@game_stats.count_of_games_by_season).to eq(Hash)
+    expected = {
+      "20122013" => 7,
+      "20132014" => 1,
+      "20142015" => 1
+    }
+    expect(@game_stats.count_of_games_by_season).to eq(expected)
   end
 
+  it "#average_goals_per_game" do
 
+    expect(@game_stats.average_goals_per_game).to eq(3.44)
+  end
+
+  it "#average_goals_per_season" do
+
+    expected = {
+      "20122013" => 3.85,
+      "20132014" => 3.0,
+      "20142015" => 1.0
+    }
+    expect(@game_stats.average_goals_per_season).to eq(expected)
+  end
 end
